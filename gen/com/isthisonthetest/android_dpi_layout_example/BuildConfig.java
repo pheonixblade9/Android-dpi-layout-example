8e1680006c77a8dfe2610f6a12d2d5854e3f89bf
Cody Herring <cody.herring@gmail.com>
2012-08-09 21:44:10 -0700

    initial commit

---
+++ gen/com/isthisonthetest/android_dpi_layout_example/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.isthisonthetest.android_dpi_layout_example;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/isthisonthetest/android_dpi_layout_example/MainActivity.java
@@ -0,0 +1,20 @@
+package com.isthisonthetest.android_dpi_layout_example;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
